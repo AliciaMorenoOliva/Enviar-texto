d8cc7e5e8ba6634efb6ed0e327f70037723ba362
aliciaMorenoOliva <lamorenna2@hotmail.es>
2019-06-04 11:22:15 +0200

    version inicial

---
+++ app/src/main/java/com/ng/a/intemwhatsapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ng.a.intemwhatsapp;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void enviar(View view) {
+
+        EditText editText = findViewById(R.id.editText);
+        String texto = editText.getText().toString();
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+    }
+
+}
